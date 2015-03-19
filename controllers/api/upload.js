c8245376f8480277b16321be43c0b1571fa02363
Matt McFadyen <mmcfadyen91@gmail.com>
2015-03-19 14:11:59 -0400

    csv is parsed via fast-csv :fire:

--- controllers/api/upload.js
+++ controllers/api/upload.js
@@ -1,8 +1,22 @@
 var router = require("express").Router();
+var fs = require("fs");
+var csv = require("fast-csv");
 
 router.post("/", function (req, res) {
-	console.log("hit server");
-	console.log(req.files, req.body);
+	// console.log("hit server");
+	// console.log(req.files.csvInput.path);
+
+	var stream = fs.createReadStream(req.files.csvInput.path);
+
+	var csvStream = csv()
+	    .on("data", function(data){
+	         console.log(data);
+	    })
+	    .on("end", function(){
+	         console.log("done");
+	    });
+
+	stream.pipe(csvStream);
 
 	res.redirect("/");
 
