7957bc69f0af8c47923fd8f633f0b0b164f1c3d7
Shane Keulen <shane.keulen@gmail.com>
2015-03-23 01:34:28 -0400

    remove files and switch to seperate folders for assets and public files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express  = require('express'),
   stylus     = require('stylus'),
   boostrap   = require('bootstrap-styl'),
   fs         = require('fs'),
+  morgan     = require('morgan'),
   multer     = require('multer'),
   gutil      = require('gulp-util'),
   path       = require('path'),
@@ -11,26 +12,29 @@ var express  = require('express'),
 
 var port = process.env.PORT || 8888;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade'); // render engine
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(multer()); // for parsing multipart/form-data
+app.use(morgan('dev'));
+
 function compile(str, path) {
   return stylus(str)
-    .set('filename', path)
+    //.set('filename', path)
     .use(boostrap());
 }
 app.use(stylus.middleware({
-  src: __dirname + '/public/css',
+  debug: true,
+  src: path.join(__dirname, 'public/css'),
   compile: compile
 }));
 
-app.use(express.static(__dirname + '/public'));
 app.use('/api/upload', require('./controllers/api/upload'));
-app.use('/', require('./controllers/static'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+//app.use('/', require('./controllers/static'));
 
+app.set('view engine', 'jade'); // render engine
+app.set('views', path.join(__dirname, 'views'));
 
 
 var server = app.listen(port, function() {
